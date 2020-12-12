10c37e7611ebac22fa75397c07ee2dadedcbcece
NGC-7841 <37933948+NGC-7841@users.noreply.github.com>
2020-12-12 15:39:57 +0800

    Add files via upload

--- Show_Rectangle.m
+++ Show_Rectangle.m
@@ -2,5 +2,5 @@ Settings;
 ro = hgtransform;
 display_rec = rectangle('Position', [-25,-5,50,10], 'Curvature', [0 0], 'edgecolor', color, 'facecolor', color, 'parent', ro, 'linewidth', 1);
 ro.Matrix = makehgtform('zrotate', deg);
-pause(5);
+pause(rectangle_time);
 delete(display_rec);
